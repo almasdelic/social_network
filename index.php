ace7ecf0f067afd2e7fba352dc594af2fd319c01
Almas <130715650+almasdelic@users.noreply.github.com>
2024-02-12 15:10:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <?php
 if(isset($_SESSION['email'])) : ?>
 
+    <?php create_post(); ?>
+    <br>
+
     <form method="POST">
         <h2>Create new post</h2>
         <br>
@@ -13,8 +16,35 @@ if(isset($_SESSION['email'])) : ?>
         <input type="submit" value="Post" name="submit">
     </form>
 
+    <hr>
+
+    <div>
+        <?php display_message(); ?>
+    </div>
+
+    <div class="posts">
+
+        <?php fetch_all_posts(); ?>
+
+    </div>
 
 <?php else : ?>
+
+    <div class="homepage">
+
+        <h1>Welcome to the Retro Social Network</h1>
+        <hr>
+        <p>Welcome to our nostalgic corner of the internet, where the digital clocks turn back to the early 2000s.
+           This social network is a haven for those who yearn for the simpler, more genuine connections of yesteryear.
+        </p>
+        <br>
+
+        <h2>Click <a href="login.php">here</a> to login</h2>
+        <br>
+
+        <img src="css/img/social.jpg" alt="">
+
+    </div>
 
 
 <?php endif; ?>
