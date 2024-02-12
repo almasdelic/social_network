5c59d7584d5cfd93cce74903a0c129a006872584
Almas <130715650+almasdelic@users.noreply.github.com>
2024-02-12 15:11:08 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,13 @@
 <?php
 include "inc/header.php";
+user_restrictions();
 ?>
 
-<?php
+<div>
+    <?php display_message(); ?>
+</div>
 
+<?php
 $user = get_user();
 echo "<img class='profile-photo' src='" . $user['profile_photo'] . "'>";
 
@@ -13,7 +17,7 @@ user_profile_image_upload();
 
     <form method="POST" enctype="multipart/form-data">
         Select image to upload:
-        <input type="file" name="profile_image_file">
+        <input type="file" name="profile_photo_file">
         <input type="submit" value="Upload Image" name="submit">
     </form>
 
